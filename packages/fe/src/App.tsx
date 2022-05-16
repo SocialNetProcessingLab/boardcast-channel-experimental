1010549f2be69b209d1a772f3dd061725bda759f
Himself65 <himself65@outlook.com>
2022-05-16 10:43:16 -0500

    feat: add basic usage for board cast message channel

--- packages/fe/src/App.tsx
+++ packages/fe/src/App.tsx
@@ -1,22 +1,40 @@
 import { Button, Typography } from '@mui/material'
-import { ItemDB } from '@nlpdev/database'
+import { AsyncCall } from 'async-call-rpc'
+import { BroadcastMessageChannel } from 'async-call-rpc/utils/web/broadcast.channel'
 import React from 'react'
 import create from 'zustand'
 
 const useStore = create(() => ({}))
 
+type Server = {
+  message(): Promise<void>
+}
+
+const server = AsyncCall<Server>(
+  {
+    message: () => {
+      console.log('hello, world!')
+      return 123
+    }
+  },
+  {
+    channel: new BroadcastMessageChannel('main')
+  }
+)
+
 export const App: React.FC = function App () {
   // ???
   useStore()
-  return (<>
-    <Typography variant='h1'>Hello world!</Typography>
-    <Button
-      onClick={() => {
-        // ???
-        ItemDB.create()
-      }}
-    >
-      Create
-    </Button>
-  </>)
+  return (
+    <>
+      <Typography variant='h1'>Hello world!</Typography>
+      <Button
+        onClick={() => {
+          server.message().then(console.log)
+          console.log('click')
+        }}>
+        Click
+      </Button>
+    </>
+  )
 }
